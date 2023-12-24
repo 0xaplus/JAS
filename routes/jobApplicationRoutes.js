2ed117f8ca88a7b64c71e95297feddef83b92bca
0xaplus <codewithaplus@gmail.com>
2023-12-24 23:05:32 +0100

    Removed/renamed

--- routes/jobApplicationRoutes.js
+++
@@ -1,9 +0,0 @@
-const jobsApplicationRoute = require('express').Router();
-const jobApplicationCtrller = require('../controllers/jobApplication.controller')
-
-jobsApplicationRoute.get('/:jobListingId/applications', jobApplicationCtrller.getJobApplications);
-jobsApplicationRoute.post('/applications', jobApplicationCtrller.createJobApplication);
-jobsApplicationRoute.put('/:id', jobApplicationCtrller.updateJobApplication);
-jobsApplicationRoute.delete('/:id', jobApplicationCtrller.deleteJobApplication);
-
-module.exports = jobsApplicationRoute; 
